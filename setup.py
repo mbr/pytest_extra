0a031508f733e01de0495ab54a88f588868a772f
Marc Brinkmann <git@marcbrinkmann.de>
2014-08-14 15:49:32 +0200

    Increased version to 0.2.dev1 after release of 0.1.
    (commit by unleash 0.5.8.dev1)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name='pytest_extra',
-    version='0.1dev',
+    version='0.2.dev1',
     description='Some helpers for writing tests with pytest.',
     long_description=read('README.rst'),
     author='Marc Brinkmann',
